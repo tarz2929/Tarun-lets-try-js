4a41a728aaf8519453d29a8b658e7a109679f825
Warren Uhrich <hello@warren.codes>
2020-08-21 11:30:59 -0600

    Add back-tick string, template literal, and concatenation examples.

--- js/scripts.js
+++ js/scripts.js
@@ -53,5 +53,50 @@ const myArray = [ // Arrays can store multiple pieces of data (including other a
  * Writing to the console.
  */
 
-console.log( "Hello to the browser console!" );
-console.log( myArray );
+console.log( "Hello to the browser console!" ); // We can output literal values.
+console.log( myArray ); // We can output variables and their contents.
+console.log( 10 % 3 ); // We can also just throw in an expression and see what it evaluates to!
+// Remember: "%" means modulous (the remainder you'd get from a division.)
+
+/**
+ * Concatenation and/vs. Template Literals
+ */
+
+// Standard concatenation w/strings.
+const myName = "Warren";
+const concattedString = "Hello, " + myName + "!";
+console.log( concattedString );
+
+// Watch out for this gotcha'! Number addition vs. concatenation.
+// They use the same operator: "+"
+console.log( 3 + 3 ); // (Number)6 - this was an addition!
+console.log( 3 + "3" ); // (String)"33" - this was a concatenation.
+console.log( 3 + 3 + "3" ); // (String)"63" - an addition of the first two... THEN concatenation!
+console.log( 3 + 3 + Number( "3" ) ); // We can use the "Number()" function in JS to convert strings to numbers.
+                                      //(if it is string featuring numeric characters.)
+console.log( String( 3 ) + String( 3 ) + "3" ); // We can use the "String()" function in JS to convert numbers (or other datatypes) to strings.
+
+console.log( parseInt( 38.957 ) ); // parseInt() and parseFloat() might feel a bit more familiar for ensuring a value will or will not have decimal points.
+
+// CONCATENATION:
+// const concattedString = "Hello, " + myName + "!";
+// console.log( concattedString );
+
+// TEMPLATE LITERAL EQUIVALENT:
+const templateLiteralString = `Hello, ${myName}!`; // Template literals MUST be inside back ticks " ` " NOT single quotes " ' ".
+console.log( templateLiteralString );
+
+// Back-tick strings are white-space sensitive!
+// This means new-lines and spaces are maintained.
+console.log( `
+This is a string in back-ticks (\`).
+It is white-space and new-line sensitive!
+Great for formatting your outputs.
+
+===
+
+An example of a use for a template literal could be...
+Our array: myArray is ${myArray.length} items long.
+Its fourth index includes the value: ${myArray[4]}
+All template literals are wrapped in: "\${}"
+` );
