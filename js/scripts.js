eccc3efd2401be8f2e3c08f7aeaf04b65b90cd19
Warren Uhrich <hello@warren.codes>
2020-08-21 13:35:38 -0600

    Add more function examples (fat-arrow.)

--- js/scripts.js
+++ js/scripts.js
@@ -150,3 +150,45 @@ console.log( add2And4 );
 console.log( myAdditionFunction( -15, 35 ) );
 
 console.log( myAdditionFunction( 3.14, 67 ) );
+
+// We can assign an "anonymous" function a name as well.
+const mySubtractionFunction = function ( num1, num2 ) {
+  const difference = num1 - num2; // "-" is only used for subtraction.
+                                  // JS automatically tries to convert to the Number datatype in this case.
+  return difference;
+}
+
+console.log( mySubtractionFunction( 100, 50 ) ); // 50
+
+// Fat-arrow function syntax, as an alternative.
+/**
+ * 1) const / let for our declaration...
+ * 2) the name of our function.
+ * 3) assignment operator...
+ * 4) parenthesis "()" with any parameters inside.
+ * 5) "fat arrow" marking the beginning of function logic.
+ * 6) Here we have two options:
+ *     i) What follows the fat arrow will simply be returned from the function.
+ *    ii) We can use curly braces to manage larger instruction sets, which will
+ *        give us more control over when / how we return from the function.
+ * NOTE: The "this" keyword is not overwritten when using fat arrow functions,
+ *       like it would be with a traditional function.
+ * NOTE: When we set up a function using an assignment, it is not "hoisted."
+ *       You cannot call upon it before its declaration. Traditional functions
+ *       ARE hoisted and can be accessed anywhere in the program if in accessible
+ *       scope.
+ */
+const myDivisionFunction = ( num1, num2 ) => num1 / num2; // See: 6.i
+
+console.log( "Division (36, 6): " + myDivisionFunction( 36, 6 ) ); // 6
+
+const myMultiplicationFunction = ( num1 = 0, num2 = 0 ) => { // See: 6.ii
+  const product = num1 * num2;
+  return product;
+};
+
+console.log( "Multiplication (3, 9): " + myMultiplicationFunction( 3, 9 ) ); // 27
+
+/**
+ * 
+ */
