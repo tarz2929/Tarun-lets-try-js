21809ac1a8620df10ca25f4f6cb7c8960a97e98c
Warren Uhrich <hello@warren.codes>
2020-08-24 09:38:50 -0600

    Add dom-events.js and test that it is loading in webpage.

---
+++ js/dom-events.js
@@ -0,0 +1,7 @@
+// First thing's first... is our file loading into the browser?!
+// alert( "DOM Events JS loaded!" );
+// console.log( "DOM Events JS loaded!" );
+// ***Normally the above gets removed after your test...
+// ***This is just for your reference!
+
+
