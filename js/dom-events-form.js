1a46fe97cb2f8309eaa0dca57f0cb9e13019cd89
Warren Uhrich <hello@warren.codes>
2020-08-24 12:09:30 -0600

    Add submit event; add logs for the event object and target.

--- js/dom-events-form.js
+++ js/dom-events-form.js
@@ -12,3 +12,23 @@ console.log( nameField );
 // Let's grab our list, so we can output hellos to the user!
 const helloList = document.querySelector( '#hello-list' );
 console.log( helloList );
+
+/**
+ * Event Listener: Form Submit
+ */
+
+// eventListener passes an argument to our function... an event object!
+helloForm.addEventListener( 'submit', ( event ) => {
+  // Stop the form from submitting! 
+  event.preventDefault(); // We don't want a new pageload!
+  console.log( event ); // The full event object.
+  console.log( event.target ); // Gets the element that the event occurred on.
+
+  // How do we get our "value" from our form field?
+
+  // How do I make a new LI element?
+
+  // How do I insert a value/text into the LI element?
+
+  // How do I add the LI element to the page (or, our list?)
+} );
