d4d0b5de6eb3cb15d2e5dbefe7e88247c2124601
Warren Uhrich <hello@warren.codes>
2020-08-24 11:46:04 -0600

    Add form; grab form using querySelector().

---
+++ js/dom-events-form.js
@@ -0,0 +1,6 @@
+// alert( "DOM Events Form Script Loaded!" );
+// Yay! Our script is loaded!
+
+// Let's grab our form... it can listen for "submit"!
+const helloForm = document.querySelector( 'form' );
+console.log( helloForm );
