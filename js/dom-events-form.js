51616eef11b767b3bb13019a5eca63d0bf9df1fb
Warren Uhrich <hello@warren.codes>
2020-08-24 11:51:16 -0600

    Collect important form / list HTML elements.

--- js/dom-events-form.js
+++ js/dom-events-form.js
@@ -4,3 +4,11 @@
 // Let's grab our form... it can listen for "submit"!
 const helloForm = document.querySelector( 'form' );
 console.log( helloForm );
+
+// Let's grab our form field, so that we can access its field value.
+const nameField = document.getElementById( 'your-name' );
+console.log( nameField );
+
+// Let's grab our list, so we can output hellos to the user!
+const helloList = document.querySelector( '#hello-list' );
+console.log( helloList );
