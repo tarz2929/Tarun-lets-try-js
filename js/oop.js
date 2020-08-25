2b433b7677c0060816bb3b75a9666771fee7aeed
Warren Uhrich <hello@warren.codes>
2020-08-25 09:58:42 -0600

    Add JSON example.

--- js/oop.js
+++ js/oop.js
@@ -10,4 +10,19 @@ const myObj = {
 // Access properties or methods with "."
 console.log( `My name is ${myObj.name}. I am ${myObj.age} years old!` );
 
+console.log( myObj );
+
 // JSON (JavaScript Object Notation)
+// Properties MUST be wrapped in double quotes to be considered valid JSON.
+// We CANNOT have methods in a JSON string/object.
+const secondPersonString = `{
+  "name": "Sarah",
+  "age": 29,
+  "hobbies": [ "Deep Sea Fishing", "Cycling" ]
+}`;
+console.log( secondPersonString );
+
+// Convert a string into a regular JS object we can use.
+const secondPersonObj = JSON.parse( secondPersonString );
+// If the string was properly formatted JSON, it should now be usable!
+console.log( secondPersonObj );
