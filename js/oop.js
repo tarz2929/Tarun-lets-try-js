b768cffccab3907f344ced006dae0ca68995ea11
Warren Uhrich <hello@warren.codes>
2020-08-25 09:47:26 -0600

    Add object example.

---
+++ js/oop.js
@@ -0,0 +1,13 @@
+// alert( "OOP.js loaded!" ); // The file is loaded if we see the alert!
+
+// Basic JS object.
+const myObj = {
+  name: "Joe",
+  age: 35,
+  hobbies: [ "Movies", "Pottery" ]
+};
+
+// Access properties or methods with "."
+console.log( `My name is ${myObj.name}. I am ${myObj.age} years old!` );
+
+// JSON (JavaScript Object Notation)
