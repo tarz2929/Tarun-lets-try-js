e447aecf78e00fe062ce393d1111e3b8b9cb1f5a
Warren Uhrich <hello@warren.codes>
2020-08-26 11:17:58 -0600

    Output our astronaut data in the browser as a list.

--- js/ajax-api.js
+++ js/ajax-api.js
@@ -19,5 +19,22 @@ fetch( 'http://api.open-notify.org/astros.json' )
   // JSON string that the API provides.) 
   .then( response => response.json() )
   // Let's check to see if our object is here!
-  .then( data => { console.log( data ); } );
-
+  .then( data => {
+    // Check data... it is an object with properties!
+    console.log( data );
+    // Grab the people from the object.
+    const people = data.people; // The array is in the "people" property.
+    console.log( people ); // This should be the array!
+    // Let's prep some HTML for output in the browser.
+    const peopleUL = document.createElement( 'UL' );
+    // Let's loop through the people, and add each one to the list...
+    for ( const person of people )
+    { // Let's set up our element.
+      const personLI = document.createElement( 'LI' );
+      // Fill it up with text...
+      personLI.textContent = `On the ${person.craft} craft, astronaut "${person.name}" is present!`;
+      // ...and send it on its way! Into the UL we prepped.
+      peopleUL.appendChild( personLI );
+    } // Don't forget to add the UL to the browser so we can see it!
+    document.body.appendChild( peopleUL );
+  } );
