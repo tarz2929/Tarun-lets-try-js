18b81211af6d278655efdb040067705b5281cd1c
Warren Uhrich <hello@warren.codes>
2020-08-26 10:48:49 -0600

    Add AJAX notes.

---
+++ js/ajax-api.js
@@ -0,0 +1,16 @@
+// alert( "AJAX API JS loaded!" ); // Check if the file is loading!
+
+/**
+ * AJAX (Asynchronous JavaScript And XML)
+ * ======================================
+ * Lets us use JavaScript to request information from separate
+ * URLs (make separate HTTP/HTTPS requests) post-initial page-
+ * load.
+ * Despite AJAX referring to XML, we are /usually/ dealing with
+ * JSON-formatted data responses here in 2020.
+ * The modern way to make AJAX requests with JS usually entails
+ * use of the built-in "fetch()" function/feature.
+ */
+
+
+ 
